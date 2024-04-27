a598333774fc0e0d099c7434b68af7a9eb81d9d8
xrm673 <rx67@cornell.edu>
2024-04-27 17:17:49 -0400

    adding relationship in db.py

--- src/db.py
+++ src/db.py
@@ -2,18 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# association_table = db.Table(
-#     "association",
-#     db.Model.metadata,
-#     db.Column("task_id",db.Integer,db.ForeignKey("tasks.id")),
-#     db.Column("category_id",db.Integer, db.ForeignKey("categories.id"))
-# )
-
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
     name = db.Column(db.String,nullable=False)
     password = db.Column(db.String,nullable=False)
+    tasks = db.relationship("Task", cascade="delete")
 
     def __init__(self,**kwargs):
         self.name = kwargs.get("name","")
@@ -23,7 +17,8 @@ class User(db.Model):
         return {
             "id": self.id,
             "name": self.name,
-            "password": self.password
+            "password": self.password,
+            "tasks": [t.serialize() for t in self.tasks]
         }
 
 
@@ -35,6 +30,7 @@ class Task(db.Model):
     priority = db.Column(db.Integer,nullable=False)
     time_to_complete = db.Column(db.String,nullable=False)
     done = db.Column(db.Boolean,nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"),nullable=False)
 
     def __init__(self,**kwargs):
         self.name = kwargs.get("description","")
@@ -42,6 +38,7 @@ class Task(db.Model):
         self.priority = kwargs.get("priority",0)
         self.time_to_complete = kwargs.get("time_to_complete","")       
         self.done = kwargs.get("done",False)
+        self.user_id = kwargs.get("user_id")
 
     def serialize(self):
         return {
@@ -50,5 +47,6 @@ class Task(db.Model):
             "deadline": self.deadline,
             "priority": self.priority,
             "time_to_complete": self.time_to_complete,
-            "done":self.done
+            "done":self.done,
+            "user_id":self.user_id
         }
